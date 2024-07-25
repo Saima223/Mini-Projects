557e8689c4e5cbe041b702c840ce49da13fba5bb
Saima223 <siddiqui22062004@gmail.com>
2024-07-25 23:59:34 +0530

    minor changes

--- Simon Game/Game.js
+++ Simon Game/Game.js
@@ -1,1 +1,26 @@
-alert("hello");
+const buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+
+function nextSequence() {
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    // Use jQuery to select the button with the same id as the randomChosenColour
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    // Play the sound for the selected button
+    makeSound(randomChosenColour);
+
+    return randomChosenColour;
+}
+
+function makeSound(name) {
+    let audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+// Start the game only after the user interacts with the page
+$(document).keypress(function() {
+    nextSequence();
+});
